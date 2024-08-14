39a504cd1b790f821aaa23a6adcb04c26a858f6b
Romyl <anonum345@gmail.com>
2024-08-14 16:40:16 +0300

    create a variable with a question about a film and an object with movies

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-"use strict";
+'use strict';
+const numberOfFilms = +prompt('Скільки фільмів ви вже глянули?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Ваш останній переглянутий фільм?', '');
+const b = +prompt('На скільки оцінити його?', '');
+const c = prompt('Ваш останній переглянутий фільм?', '');
+const d = +prompt('На скільки оцінити його?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 const category = 'toys';
 console.log(`https:://someurl.com/${category}`);
-
+ */
 
 
 
