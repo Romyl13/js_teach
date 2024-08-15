dac7d0873383dce555e67a432c51539724113819
Romyl <anonum345@gmail.com>
2024-08-15 10:24:51 +0300

    performed 2 practical and used loops and conditions for repeated questions and evaluation of the list of watched movies of the user

--- js/script.js
+++ js/script.js
@@ -1,6 +1,89 @@
 'use strict';
 
 
+
+const numberOfFilms = +prompt('Скільки фільмів ви вже глянули?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt('Ваш останній переглянутий фільм?', '');
+    const b = +prompt('На скільки оцінити його?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done')
+    } else {
+        console.log('Error')
+        i--;
+    }
+} */
+
+/* let i = 0;
+while (i < 2) {
+    const a = prompt('Ваш останній переглянутий фільм?', '');
+    const b = +prompt('На скільки оцінити його?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done')
+        i++
+    } else {
+        console.log('Error')
+        i--;
+    }
+} */
+
+let i = 1;
+do {
+    const a = prompt('Ваш останній переглянутий фільм?', '');
+    const b = +prompt('На скільки оцінити його?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done')
+        i++
+    } else {
+        console.log('Error')
+        i--;
+    }
+} while (i < 2);
+
+
+
+
+
+/* ############################################################################## */
+if (personalMovieDB.count < 10) {
+    console.log('Мало фільмів глянув'); 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('ю ар класікал зрітель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('ТИ ДЕМОН');
+} else {
+    console.log('Error')
+}
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 let result = '';
 const length = 7;
 
@@ -13,7 +96,7 @@ for (let i = 1; i < length; i++) {
 };
 
 console.log(result);
-
+ */
 
 
 
